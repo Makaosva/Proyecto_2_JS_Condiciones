e1cae0f128f7dc131d5e41e08ca5f8beb4dbdd8e
Makaosva <maka.osses@gmail.com>
2024-04-07 00:14:25 -0400

    feat/arreglo numeros negativos en problema 2

--- assets/js/scripts2.js
+++ assets/js/scripts2.js
@@ -4,7 +4,9 @@ function verificarSrickers() {
   const inputNumber3 = Number(document.querySelector("#inputNumber3").value);
   const Total = inputNumber1 + inputNumber2 + inputNumber3;
   const mensaje = document.getElementById("mensaje");
-  if (Total <= 10) {
+  if (inputNumber1 < 0 || inputNumber2 < 0 || inputNumber3 < 0) {
+    mensaje.innerHTML = "<strong>El numero de stickers seleccionado no puede ser menos que cero</strong>";
+  } else if (Total <= 10) {
     mensaje.innerHTML = "Llevas " + Total + " stickers";
   } else {
     mensaje.innerHTML = "<strong>Llevas demasiados stickers</strong>";
